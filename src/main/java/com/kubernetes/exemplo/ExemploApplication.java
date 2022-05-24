39c0aa874dbd8f0332e2e40927e071193374a2b7
Guilherme Felipe Campos <gui_fc@hotmail.com>
2022-05-24 14:59:01 -0300

    first commit

---
+++ src/main/java/com/kubernetes/exemplo/ExemploApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetes.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploApplication.class, args);
+	}
+
+}
